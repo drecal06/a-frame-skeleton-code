e22edb8964587078baba37eb0db179068526a09f
Dre cal <baseballdre@icloud.com>
2023-12-05 17:57:46 +0000

    ...

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function switchScene() {
+    const scene1 = document.getElementById("scene1")
+    const scene2 = document.getElementById("scene2")
 
+    if scene1.style.display !== "none":
+        scene1.style.display = "none"
+        scene2.style.display = ""
+}
