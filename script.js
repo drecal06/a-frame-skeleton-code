9d82c5b45937cd68fda932a3feb32880f11b5c0a
Zachary Huang <zadahu@gmail.com>
2023-12-14 21:07:10 +0000

    button

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function switchScene() {
     const scene1 = document.getElementById("scene1")
     const scene2 = document.getElementById("scene2")
 
-    if scene1.style.display !== "none":
+    if (scene1.style.display !== "none"){
         scene1.style.display = "none"
         scene2.style.display = ""
+    }
+    else if (scene2.style.display !== "none"){
+        scene2.style.display = "none"
+        scene1.style.display = ""
+    }
 }
